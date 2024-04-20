478a8a58f34f5168f4a73e60f458476f67094341
David Bezuidenhout <tinuva@ipv6.za.org>
2024-04-20 21:47:41 +0200

    color tweaks

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,6 +59,11 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: false,
+      respectPrefersColorScheme: true, // Whether to use the prefers-color-scheme media-query, using user system preferences, instead of the hardcoded defaultMode.
+    },
     navbar: {
       title: 'LinuxGeek.za.net',
       logo: {
